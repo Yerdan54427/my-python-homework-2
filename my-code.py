aa52b4a7a40a51a2ceac76b47bf39a416708a4ab
Yerdan <Yerdan@qq.com>
2026-03-26 17:51:00 +0800

    feat: 实现 ExamSystem 的 __init__ 和 load_students 方法

--- my-code.py
+++ my-code.py
@@ -11,3 +11,30 @@ class Student:
             f"学号：{self.student_id}，姓名：{self.name}，性别：{self.gender}，"
             f"班级：{self.class_name}，学院：{self.college}"
         )
+
+
+class ExamSystem:
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.students = []
+
+    def load_students(self):
+        try:
+            with open(self.file_name, "r", encoding="utf-8") as file:
+                self.students = []
+                next(file, None)
+
+                for line in file:
+                    line = line.strip()
+                    if not line:
+                        continue
+
+                    parts = line.split("\t")
+                    if len(parts) != 6:
+                        continue
+
+                    _, name, gender, class_name, student_id, college = parts
+                    student = Student(student_id, name, gender, class_name, college)
+                    self.students.append(student)
+        except FileNotFoundError:
+            print(f"文件不存在：{self.file_name}")
